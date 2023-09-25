9344b4c1b88e0d054cf95dcbd1268d36993ee204
Oleksii Horban <lehagorban93@gmail.com>
2023-09-25 21:57:09 +0300

    change url

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper';
 
-const baseUrl = 'http://localhost:3001/api';
+const baseUrl = 'https://fullstack-transaction-server.onrender.com/api';
 
 export const instanse = axios.create({
   baseURL: baseUrl,
