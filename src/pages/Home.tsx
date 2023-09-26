55bb3c926e38d8f31cd11ae5bf7d0c8a1b007192
Oleksii Horban <lehagorban93@gmail.com>
2023-09-26 20:38:03 +0300

    fix title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import { FC } from 'react';
 const Home: FC = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center  text-white">
-      <h1 className="mb-4 text-6xl">Welcome to Transactions!</h1>
+      <h1 className="mb-4 text-center text-5xl md:text-6xl">
+        Welcome to Transactions!
+      </h1>
       <p className="max-w-md text-center text-xl">
         Track your income and expenses with ease and style. Get started by
         adding a new transaction.
