ccc2d91f2894f015ff1de973a4e84fbfd55365ca
Oleksii Horban <lehagorban93@gmail.com>
2023-09-26 22:51:45 +0300

    fix form

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -50,13 +50,13 @@ const Auth: FC = () => {
   };
 
   return (
-    <div className="mt-40 flex flex-col items-center justify-center bg-slate-900 text-white">
+    <div className="mx-auto mt-40 flex max-w-sm flex-col bg-slate-900 text-white">
       <h1 className="mb-10 text-center text-xl">
         {isLogin ? 'Login' : 'Registration'}
       </h1>
       <form
         onSubmit={isLogin ? loginHandler : registrationHandler}
-        className="mx-auto flex w-1/3 flex-col gap-5"
+        className="flex flex-col gap-5"
       >
         <input
           onChange={(e) => setEmail(e.target.value)}
